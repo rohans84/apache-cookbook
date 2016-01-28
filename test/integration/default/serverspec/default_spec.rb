2ca9534a90e018f7572faa504464e655a1171374
John Doe <jdoe@example>
2016-01-28 21:44:46 +0000

    added tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe 'apache::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+    describe port(80) do
+	it { should be_listening }
+    end
+
+    describe command('curl http://localhost') do
+	its(:stdout) { should match /Hello, world!/ }
+    end
 end
